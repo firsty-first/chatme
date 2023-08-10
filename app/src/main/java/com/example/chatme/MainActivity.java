2e2a026c3a1c8180ec1527f7ea00ebb62f13d677
Gaurav Singh <gks.21u10252@btech.nitdgp.ac.in>
2023-08-11 01:00:38 +0530

    added signup

--- app/src/main/java/com/example/chatme/MainActivity.java
+++ app/src/main/java/com/example/chatme/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chatme;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -19,22 +20,31 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
   private   FirebaseAuth firebaseAuth;
   FirebaseDatabase database;
+  ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        getSupportActionBar().hide();
+       // getSupportActionBar().hide();
+        progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Creating acccount");
+        progressDialog.setMessage("creatign your acount");
         binding.btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressDialog.show();
                 firebaseAuth.createUserWithEmailAndPassword(binding.editTextTextEmailAddress.getText().toString(),
                         binding.editTextTextPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
                         if(task.isSuccessful())
                         {
+                            UserModel user=new UserModel(binding.edittextName.toString(),binding.editTextTextEmailAddress.toString(),binding.editTextTextPassword.toString());
+                        String id=task.getResult().getUser().getUid();
+                        database.getReference().child("user").child(id).setValue(user);
                             Toast.makeText(MainActivity.this, "User created successful", Toast.LENGTH_SHORT).show();
                         }
                         else
